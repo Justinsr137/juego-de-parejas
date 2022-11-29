c7a837df818c91920898cc5f905f01febf74c340
Justinsr137 <106892415+Justinsr137@users.noreply.github.com>
2022-11-30 00:07:17 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ let timer = 30;
 let mostrarTiempo = document.getElementById('t-restante');
 let tiempoRegresivo = null;
 let timerInicial = 30;
+let titulo = document.querySelector('.titulo');
+
+let winaudio = new Audio('./sound/win.wav') 
+let loseaudio = new Audio('./sound/lose.wav') 
+let clickaudio = new Audio('./sound/click.wav') 
+let wrongaudio = new Audio('./sound/wrong.wav') 
+let rightaudio = new Audio('./sound/right.wav') 
 
 let numeros = [1,1,2,2,3,3,4,4,5,5,6,6,7,7,8,8];
 numeros = numeros.sort(()=>{return Math.random()-0.5});
@@ -29,13 +36,14 @@ function destapar(id){
     if(tarjetasDestapadas == 1){
         tarjeta1 = document.getElementById(id);
         primerResultado = numeros[id];
-        tarjeta1.innerHTML = primerResultado;
+        tarjeta1.innerHTML = `<img src="./images/${primerResultado}.png">`;
+        clickaudio.play();
 
         tarjeta1.disabled = true;
     }else if(tarjetasDestapadas == 2){
         tarjeta2 = document.getElementById(id);
         segundoResultado = numeros[id];   
-        tarjeta2.innerHTML = segundoResultado;
+        tarjeta2.innerHTML = `<img src="./images/${segundoResultado}.png">`;
 
         tarjeta2.disabled = true;
 
@@ -47,14 +55,17 @@ function destapar(id){
 
             aciertos++;
             mostrarAciertos.innerHTML = `Aciertos: ${aciertos}`;
+            rightaudio.play();
 
             if(aciertos == 8){
+                winaudio.play();
                 clearInterval(tiempoRegresivo);
                 mostrarAciertos.innerHTML = `Eri gey?`
                 mostrarTiempo.innerHTML = `Felicitaciones, tu tiempo fue: ${timerInicial - timer } segundos`
                 mostrarMovimientos.innerHTML = `Pedro`
             }
         }else{
+            wrongaudio.play();
             setTimeout(()=>{
                 tarjeta1.innerHTML = ' '
                 tarjeta2.innerHTML = ' '
@@ -73,6 +84,8 @@ function contarTiempo(){
         if(timer == 0){
             clearInterval(tiempoRegresivo);
             bloquearTarjetas();
+            loseaudio.play();
+            titulo.innerHTML = 'PERDISTE'
         }
     }, 1000);
 }
@@ -80,7 +93,7 @@ function contarTiempo(){
 function bloquearTarjetas(){
     for (let index = 0; index <= 15; index++) {
         let tarjetaBloqueada = document.getElementById(index);
-        tarjetaBloqueada.innerHTML = numeros[index];
+        tarjetaBloqueada.innerHTML = `<img src="./images/${numeros[index]}.png">`;
         tarjetaBloqueada.disabled = true;
         
     }
